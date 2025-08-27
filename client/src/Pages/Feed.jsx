f9e3aee800dc74e03413bb6e39615edc3f349cd9
shefali1503 <shefalisharmaa4455@gmail.com>
2025-08-27 20:39:59 +0530

    Added storybar

--- client/src/Pages/Feed.jsx
+++ client/src/Pages/Feed.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../Components/Loading'
+import { useEffect } from 'react'
+import StoriesBar from '../Components/StoriesBar'
 
 const Feed = () => {
-  return (
-    <div>
-      
+
+  const [feeds, setfeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchFeeds()
+  }, [])
+
+  return !loading ?(
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list */}
+      <div>
+        <StoriesBar />
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+      </div>
+
+      {/* Right Sidebar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Feed
