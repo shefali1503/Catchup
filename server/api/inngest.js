e61b8cc33a6ce1c39f2fef65398100d08fb42d68
shefali1503 <shefalisharmaa4455@gmail.com>
2025-08-30 10:24:57 +0530

    made api/inngest file

--- server/api/inngest.js
+++ server/api/inngest.js
@@ -1,11 +1,13 @@
 import { serve } from "inngest/vercel";
-import { inngest, functions } from "../inngest/index.js";
-import connectDB from "../configs/db.js";
+import { inngest, functions } from "../../inngest/index.js";
+import connectDB from "../../configs/db.js";
 
-// Ensure DB connection before running any function
-await connectDB();
-
+// Export handler for Vercel
 export default serve({
   client: inngest,
   functions,
+  onRequest: async () => {
+    // Ensure DB connection before running any function
+    await connectDB();
+  },
 });
